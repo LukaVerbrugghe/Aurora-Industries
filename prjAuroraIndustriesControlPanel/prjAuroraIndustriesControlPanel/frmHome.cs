097ac242709e8ede3bfb2b14f614e52fa8aef537
professorVerbrugghe <lotrdetailsys@gmail.com>
2024-01-26 20:28:17 +0100

    First Commit he godverdomme

--- prjAuroraIndustriesControlPanel/prjAuroraIndustriesControlPanel/frmHome.cs
+++ prjAuroraIndustriesControlPanel/prjAuroraIndustriesControlPanel/frmHome.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace prjAuroraIndustriesControlPanel
 {
-    public partial class Form1 : Form
+    public partial class frmHome : Form
     {
-        public Form1()
+        public frmHome()
         {
             InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            
+            string userName = Interaction.InputBox("Please provide a log in:", "User Name");
+            DialogResult loginAnser = MessageBox.Show("You where automaticly logged in by Aurora Security Protocol (ASP) using your known credentials. \nClick ok to continue or cancel to cancel to log in request.\n\nWarning: Cancelling a log in request to often may trigger a security check.", "Notice of log in", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+
+            //check the anser of the user
+            if (loginAnser == DialogResult.OK)
+            {
+                //log in
+                frmNavigatie frmNavigatie = new frmNavigatie();
+                frmNavigatie.Show();
+            }
+            else if (loginAnser == DialogResult.Cancel)
+            {
+                //do nothing
+                MessageBox.Show("Log in cancelled.");
+            }
+            else
+            {
+                //er is iets srs misgegaan
+                MessageBox.Show("Fatal error: Dialog Result was not OK or CANCEL. Contact Aurora Industries.", "Error: Dialog Result did not match", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
--- prjAuroraIndustriesControlPanel/prjAuroraIndustriesControlPanel/frmNavigatie.Designer.cs
+++ prjAuroraIndustriesControlPanel/prjAuroraIndustriesControlPanel/frmNavigatie.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace prjAuroraIndustriesControlPanel
 {
-    partial class frmHome
+    partial class frmNavigatie
     {
         /// <summary>
         /// Required designer variable.
@@ -29,42 +29,19 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.btnExit = new System.Windows.Forms.Button();
-            this.btnLogIn = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(146, 77);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(446, 25);
+            this.label1.Size = new System.Drawing.Size(665, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "AURORA INDUSTRIES CONTROL PANEL";
-            // 
-            // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(12, 402);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(107, 36);
-            this.btnExit.TabIndex = 1;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
-            // btnLogIn
-            // 
-            this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogIn.ForeColor = System.Drawing.Color.Red;
-            this.btnLogIn.Location = new System.Drawing.Point(43, 128);
-            this.btnLogIn.Name = "btnLogIn";
-            this.btnLogIn.Size = new System.Drawing.Size(690, 238);
-            this.btnLogIn.TabIndex = 2;
-            this.btnLogIn.Text = "LOG IN";
-            this.btnLogIn.UseVisualStyleBackColor = true;
-            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
             // 
             // label2
             // 
@@ -72,20 +49,29 @@
             this.label2.Location = new System.Drawing.Point(478, 428);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(310, 13);
-            this.label2.TabIndex = 3;
+            this.label2.TabIndex = 5;
             this.label2.Text = "©AURORA INDUSTRIES, IMPERIAL SCIENCE FOUNDATION";
             // 
-            // frmHome
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 402);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(107, 36);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmNavigatie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.btnLogIn);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.label1);
-            this.Name = "frmHome";
-            this.Text = "Homepage - AURORA INDSUTRIES";
+            this.Name = "frmNavigatie";
+            this.Text = "Navigatie";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,9 +80,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.Button btnLogIn;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExit;
     }
 }
-
